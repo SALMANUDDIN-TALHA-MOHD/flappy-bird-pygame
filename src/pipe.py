abbf6fb48dfe8df845535731722ebe72f871d84d
SALMANUDDIN TALHA MOHD <mohdsalmanuddintalha@gmail.com>
2026-03-28 14:29:07 -0500

    Created PipeManager class to handle multiple pipes

--- src/pipe.py
+++ src/pipe.py
@@ -63,3 +63,46 @@ class Pipe:
         min_gap_y = 150
         max_gap_y = screen_height - 150
         return random.randint(min_gap_y, max_gap_y)
+    
+class PipeManager:
+    """
+    Manages multiple pipes - spawning, updating, removing
+    """
+    def __init__(self, screen_width, screen_height):
+        """Initialize pipe manager"""
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.pipes = []
+        
+        # Spawning settings
+        self.spawn_distance = 400  # Distance between pipes
+        self.next_spawn_x = screen_width + 200  # First pipe position
+        
+    def update(self):
+        """Update all pipes"""
+        # Move all pipes
+        for pipe in self.pipes:
+            pipe.update()
+        
+        # Remove pipes that are off-screen
+        self.pipes = [pipe for pipe in self.pipes if not pipe.is_off_screen()]
+        
+        # Spawn new pipe if needed
+        if len(self.pipes) == 0 or self.pipes[-1].x < self.next_spawn_x:
+            self.spawn_pipe()
+            
+    def spawn_pipe(self):
+        """Create a new pipe at the right edge"""
+        gap_y = Pipe.generate_random_gap_y(self.screen_height)
+        new_pipe = Pipe(self.screen_width, gap_y)
+        self.pipes.append(new_pipe)
+        self.next_spawn_x = new_pipe.x + self.spawn_distance
+        
+    def draw(self, screen):
+        """Draw all pipes"""
+        for pipe in self.pipes:
+            pipe.draw(screen)
+            
+    def get_pipes(self):
+        """Return list of all active pipes"""
+        return self.pipes
